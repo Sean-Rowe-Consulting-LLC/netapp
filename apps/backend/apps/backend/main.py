4d8467fa01ab162ad4072a8c39f6c2579d9b79f1
Sean Rowe <sean@seanrowe.com>
2024-01-15 11:20:47 -0600

    Add and format new content in README.md, create new pyproject.toml and poetry.lock
    Changes mainly focus on content in README.md, reformatted the JSON object and added more structure for the project requirements. New files, pyproject.toml and poetry.lock, have been created. Minor changes in the main.py file, replacing some function names and imports.

--- apps/backend/apps/backend/main.py
+++ apps/backend/apps/backend/main.py
@@ -1,11 +1,14 @@
+from typing import Union
+
+from fastapi import FastAPI
+
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-  return {"Hello": "World"}
+@app.get("/server/costs/{item_id}")
+def get_server_cost(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-  return {"item_id": item_id, "q": q}
+@app.get("/server/costs")
+def
