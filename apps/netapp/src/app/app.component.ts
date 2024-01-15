effd5a8d40d1833ea725cd4337fa7dac4e521090
Sean Rowe <sean@seanrowe.com>
2024-01-15 13:10:41 -0600

    Add and format new content in README.md, create new pyproject.toml and poetry.lock
    Changes mainly focus on content in README.md, reformatted the JSON object and added more structure for the project requirements. New files, pyproject.toml and poetry.lock, have been created. Minor changes in the main.py file, replacing some function names and imports.

--- apps/netapp/src/app/app.component.ts
+++ apps/netapp/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { StoreModule } from '@ngrx/store';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, StoreModule.forRoot({})],
   selector: 'netapp-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'netapp';
